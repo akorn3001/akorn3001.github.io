f6aaffb0567337aab1722a24c4727489282fdf66
Alex Kornfeld <akorn3000@gmail.com>
2018-08-20 13:21:08 -0400

    Move css files into styles folder

--- portfolio-revamp/src/index.js
+++ portfolio-revamp/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Root from './root';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+
+  ReactDOM.render(
+    <Root />, root);
+    registerServiceWorker();
+});
