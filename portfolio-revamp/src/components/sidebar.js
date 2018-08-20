f99293df91ef29811c9afd44dbdbd9f6e52670a9
Alex Kornfeld <akorn3000@gmail.com>
2018-08-20 14:16:35 -0400

    Add height to sidebar element to enable side scrolling

--- portfolio-revamp/src/components/sidebar.js
+++ portfolio-revamp/src/components/sidebar.js
@@ -4,16 +4,36 @@ import Headshot from '../alex_pic.png';
 const Sidebar = () => {
   return (
     <div id="sidebar">
-      <nav>
-        <ul>
-          <li>
-            <img className="headshot" src={Headshot} />
-          </li>
-        </ul>
-      </nav>
-      <p>
-        a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A a A
-      </p>
+      <div className="inner">
+        <nav>
+          <ul>
+            <li>
+              <img className="headshot" src={Headshot} />
+            </li>
+
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+
+            <li>
+              <a href="#resume">Resume</a>
+            </li>
+
+            <li>
+              <a href="#education">Education</a>
+            </li>
+
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
     </div>
   );
 };
