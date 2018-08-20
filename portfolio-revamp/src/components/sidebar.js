63e14f77ac5c589d6d38619dbb19eb24cc794129
Alex Kornfeld <akorn3000@gmail.com>
2018-08-20 13:06:37 -0400

    Create jshintrc file at root of project, create sidebar component

---
+++ portfolio-revamp/src/components/sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Headshot from '../alex_pic.png';
+
+const Sidebar = () => {
+  return (
+    <div id="sidebar">
+      <nav>
+        <ul>
+          <li>
+            <img className="headshot" src={Headshot} />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
